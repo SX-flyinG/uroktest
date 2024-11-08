d25c4e01b9a38c698bfca922c8b018c4f3845df4
Burya_ei79 <Burya_ei79@8-8>
2024-11-08 18:36:24 +0200

    add surname field to student's class

--- provtikal/provtikal.cpp
+++ provtikal/provtikal.cpp
@@ -4,32 +4,61 @@ using namespace std;
 
 class Student {
 	string name;
+	string surname;
 	int age;
 public :
 	Student() {
 		name = "Botyslav";
+		surname = "Linchevskiy";
 		age = 22;
 	}
 	Student(string name, int age) {
 		this->age = age;
+		this->surname = surname;
 		this->name = name;
 	}
 	Student(const Student& obj) {
 		this->name = obj.name;
 		this->age = obj.age;
+		this->surname = obj.surname;
 	}
 
 	void PrintInfoAboutStudent() {
 		cout << "Name of student : " << this->name << endl;
+		cout << "Surname of Student : " << this->surname << endl;
+
 		cout << "age of student : " << this->age << endl;
 	}
+		
 
 	void ChangeInfoAbouStudent() {
 		cout << "Enter a new name of student  : ";
 		cin >> this->name;
 		cout << "Enter a new age of student : ";
 		cin >> this->age;
+		cout << "Enter a new surname of student : ";
+		cin >> this->surname;
 	}
+
+	string GetName() {
+		return this->name;
+	}
+	string GetSurname() {
+		return this->surname;
+	}
+	int GetAge() {
+		return age;
+	}
+	void GetName(string name) {
+		this->name = name;
+	}
+	void GetSurname(string surname) {
+		this->surname;
+	}
+	void GetAge(int age) {
+		this->age = age;
+	}
+
 };
 
 int main()
